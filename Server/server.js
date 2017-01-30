113a491caac90ba670d34a218a61a2fff51f2243
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-29 21:12:07 -0800

    Spin up server to manage http requests

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,11 @@
-server.js
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../Client')));
+
+app.listen(8080, () => console.log('App listening on 8080'));
