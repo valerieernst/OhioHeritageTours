6da7541d8572f0d85a4d9b9a16c23aef8eca12b9
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-02-01 15:47:19 -0800

    Complete front page and linking

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, '../dist')));
+
 app.use(express.static(path.join(__dirname, '../Client')));
+
+app.get('/about', function(req, res) {
+  res.send('about');
+})
+
+
 
 app.listen(8080, () => console.log('App listening on 8080'));
